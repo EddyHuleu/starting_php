5873802e8af6a8f2ec7ca8271574ccc935820389
Eddy Huleu <eddy.huleu@live.fr>
2017-03-07 16:54:28 +0100

    exo3

--- condition.php
+++ condition.php
@@ -7,12 +7,22 @@
  //	echo 'Vous êtes majeur.';
 //}
 
-$iseasy = true;
+// $iseasy = true;
 
-if($iseasy = true){
-	echo 'good';
+//if($iseasy = true){
+	//echo 'good';
+//}else{
+//	echo 'c difficile';
+//}
+
+$age = 15;
+$genre = 'Femme';
+
+if($age < 18){
+	if($genre){
+	echo 'Vous êtes un '.$genre.' mineur.';
+	}
 }else{
-	echo 'c difficile';
+ 	echo 'Vous êtes un '.$genre.' majeur.';
 }
-
 ?>
