0205afd6c4927932f2fe278e79a831b06472397c
Eddy Huleu <eddy.huleu@live.fr>
2017-03-07 14:01:24 +0100

    exo3

--- variable.php
+++ variable.php
@@ -4,13 +4,24 @@
 
 //echo($nom);
 
-?>
 
-<?php
+//$nom = 'Huleu';
+//$prenom = 'Eddy';
+//$age = 19;
 
-$nom = 'Huleu';
-$prenom = 'Eddy';
-$age = 19;
+//echo "$nom " , "$prenom " , "$age "; 
 
-echo "$nom " , "$prenom " , "$age ";
+
+$km = 1;
+
+echo "$km";
+
+$km = 3;
+
+echo "$km";
+
+$km = 125;
+
+echo "$km";
+
 ?>
