8598e97ae60f7046065225ed1818524d673c31f4
Eddy Huleu <eddy.huleu@live.fr>
2017-03-08 13:44:52 +0100

    tableaux exo1/2

--- tableaux.php
+++ tableaux.php
@@ -2,28 +2,13 @@
 
 //EXO 1:
 
-/*$array = array(
-    1    => "Janvier",
-   	2    => "Fevrier",
-   	3    => "Mars",
-   	4    => "Avril",
-   	5    => "Mai",
-   	6    => "Juin",
-   	7    => "Juillet",
-   	8    => "Août",
-   	9    => "Septembre",
-   	10    => "Octobre",
-   	11    => "Novembre",
-   	12    => "Decembre",
-);
-var_dump($array);*/
+/*$array = array("Janvier", "Fevrier", "Mars", "Avril", "Mai", "Juin", "Juillet", "Août", "Septembre", "Octobre", "Novembre", "Decembre");
+echo $array;*/
 
 
 //EXO 2:
 
-$array = array(
-   	3    => "Mars",
-);
-var_dump($array);
+$array = array("Janvier", "Fevrier", "Mars", "Avril", "Mai", "Juin", "Juillet", "Août", "Septembre", "Octobre", "Novembre", "Decembre");
+echo $array[3];
 
 ?>
