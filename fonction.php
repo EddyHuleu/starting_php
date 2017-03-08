e16f3688295a29ac6c737a702fb0a95e18e999f9
Eddy Huleu <eddy.huleu@live.fr>
2017-03-08 11:23:55 +0100

    exo2

--- fonction.php
+++ fonction.php
@@ -10,6 +10,16 @@
 
 //EXO 2:
 
-//
+//Faire une fonction qui prend en parametre une chaine de caractere et qui retourne cette meme chaine de caractere.
+
+//function teusha($chaine){
+//	$chaine = 'teubi en i'; 
+//	return $chaine;
+//	echo 'teusha';
+//}
+
+//EXO 3:
+
+//Faire une fonction qui prend deux chaine de caractere et qui renvoie la concaténation de ses deux chaines.
 
 ?>
