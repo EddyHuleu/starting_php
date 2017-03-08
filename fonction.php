6a36b7dbf6bbed3ac2a3c3513865391b9af185e4
Eddy Huleu <eddy.huleu@live.fr>
2017-03-08 11:42:45 +0100

    exo4

--- fonction.php
+++ fonction.php
@@ -30,9 +30,28 @@
 //}
 //	echo 'oui';
 
+
 //EXO 4:
 
-// Faire une fonction qui prend en parametre deux nombres, la fonction doit retourné 
+// Faire une fonction qui prend en parametre deux nombres, la fonction doit retourné le premier nombre et plus grand si le premier est plus grand que le deuxieme, le premier nombre est plus petit si le premier nombre etp lus petit que le deuxieme, les deux nombres sont identique si les deux nobmres sont égaux.
 
+//function lesnombres($nombre1, $nombre2){
+//	$nombre1 = 10;
+//	$nombre2 = 20;
+//	if($nombre1 > $nombre2){
+//		echo '$nombre1';	
+//	}else{
+//		echo '$nombre2';
+//	}
+//	if ($nombre1 < $nombre2) {
+//		echo '$nombre2';
+//	}else{
+//		echo '$nombre1';
+//	}
+//}
+
+//EXO 5:
+
+//Faire une fonction qui prend en  parametre un nombre et une chaine de caractere et qui renvoie la concaténation de ses deux parametres.
 
 ?>
