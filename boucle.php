77432e12ae48d504da654852e86b48c06c4b0245
Eddy Huleu <eddy.huleu@live.fr>
2017-03-08 10:01:15 +0100

    exo2

--- boucle.php
+++ boucle.php
@@ -4,11 +4,28 @@
 
 //Crée une variable et l'init a 0, tant que cette variable n'atteint pas 10 il faut l'afficher l'incrémenter
 
-$i = 0;
+//$i = 0;
 
-while($i <= 10) {
-	echo ($i);
-	$i++;
+//while($i <= 10) {
+//	echo ($i);
+//	$i++;
+//}
+
+//EXO 2:
+
+//Crée deux variables initialier la premiere a 0 et la deuxieme entre 1 et 100 tant que la premiere variable est pas superieur a 20 multiplier la premiere a la deuxieme afficher le resultat incrementer la premiere variable
+
+$nombre1 = 0;
+$nombre2 = 40;
+
+while($nombre1 <= 20) {
+	$nombre1++;
+	echo ($nombre1*$nombre2);
+
 }
+
+//EXO 3:
+
+//Crée deux variables initialiser la premiere a 100 et la deuxieme entre 1 et 100 tant que la premiere variable n'est pas inferieur ou egal a 10 multiplier la premiere variable avec la deuxieme afficher le resultat decrementer la premiere variable.
 
 ?>
