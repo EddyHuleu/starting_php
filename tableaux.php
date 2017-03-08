f677ce949e60309cbdb2d22c87d721a1713e2c4d
Eddy Huleu <eddy.huleu@live.fr>
2017-03-08 13:49:14 +0100

    exo3

--- tableaux.php
+++ tableaux.php
@@ -3,12 +3,20 @@
 //EXO 1:
 
 /*$array = array("Janvier", "Fevrier", "Mars", "Avril", "Mai", "Juin", "Juillet", "Août", "Septembre", "Octobre", "Novembre", "Decembre");
-echo $array;*/
+var_dump($array);*/
 
 
 //EXO 2:
 
+/*$array = array("Janvier", "Fevrier", "Mars", "Avril", "Mai", "Juin", "Juillet", "Août", "Septembre", "Octobre", "Novembre", "Decembre");
+echo $array[3];*/
+
+
+//EXO 3:
+
 $array = array("Janvier", "Fevrier", "Mars", "Avril", "Mai", "Juin", "Juillet", "Août", "Septembre", "Octobre", "Novembre", "Decembre");
-echo $array[3];
+echo $array[5];
+
+
 
 ?>
