0f741726f0d3b3c9bf2165f8e6741fa8e056ce8f
Eddy Huleu <eddy.huleu@live.fr>
2017-03-10 14:19:20 +0100

    datemaj

--- date.php
+++ date.php
@@ -2,9 +2,13 @@
 //$date = strftime("%A %e %B %Y %H:%M");
 //echo ''.$date.'';
 
-echo time();
-echo mktime(15, 34, 12, 03, 2, 2016);
+//echo time();
+//echo mktime(15, 34, 12, 03, 2, 2016);
 
+$mai = mktime(0, 0, 0, 5, 16, 2016);
+$jour = time();
+$calcul = ($jour-$mai)/86400;
+echo 'Le nombre de jour entre aujourd\'hui et le 16 mai est '.$calcul.'';
 
 
 ?>
